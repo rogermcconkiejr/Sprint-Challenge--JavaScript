4bcba453572bee8ff957907c8a0dbf4797d6e98e
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-07-26 09:37:09 -0600

    objects finished

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,5 +1,3 @@
-import { createBrotliCompress } from "zlib";
-
 // ==== Callbacks ====  
 
 /* Step 1: Create a higher-order function that accepts a callback
@@ -18,22 +16,22 @@ function divideNums (num1, num2, callback){
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-function add (num1, num2, cb){
-  return cb(num1+num2);
+function consume (num1, num2, add){
+  return add(num1+num2);
 }
 
-function multiply (num1, num2, cb){
-  return cb(num1*num2);
+function consume (num1, num2, multiply){
+  return multiply(num1*num2);
 }
 
-function greeting (firstName, lastName){
+function consume (firstName, lastName){
   return `Hello ${firstName} ${lastName}, nice to meet you!`
 }
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
